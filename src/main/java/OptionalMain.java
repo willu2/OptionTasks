98f713ad820f7afffca8c1bd48759c9ff01b2db1
willu2 <bundestag2@gmail.com>
2019-05-29 12:57:32 +0300

    end part two

--- src/main/java/OptionalMain.java
+++ src/main/java/OptionalMain.java
@@ -12,119 +12,25 @@ public class OptionalMain {
 
 //===============================================================//
         OptionalTask2 task2 = new OptionalTask2();
-        //task2.firstMaxMin2_1();
-        //task2.firstMaxMin2_3();
-        task2.digitMinFind2_4();
+        //task2.firstMaxMin2_1_2();
+        //task2.MidLenMin2_3();
+        //task2.digitMinFind2_4();
+        //task2.increasingNumbers2_6();
+        //task2.differentDigitFind2_7();
 
 
-        //characterFind();
 
-        //digitFind();
-        //searchDigitPlusOne();
 
-       // findValueWithMinSameChars();
+
     }
 
 
-//=========================================second================================
+//=========================================third================================
 
 
 
 
 
-
-    public static void characterFind(){
-
-        String s = "aaaaabkjvbvbvbzjfdhvjfb fdjbv kjfvdbdvbjbfdjkvnkjjhlkglkhsfdjghfkjdhfdnkjvnk";
-        ArrayList<String>  strings = new ArrayList<>();
-        String temp = "";
-        Character character = 'b';
-        for (int i = 0; i < s.length(); i++) {
-//            character = s.charAt(i);
-            if(character == s.charAt(i)){
-                temp += s.charAt(i);
-                //s.
-            }
-           // System.out.println(temp + "  " + temp.length() + " >>" + temp.charAt(0));
-        }
-
-        String input = s.toLowerCase();// Make your input toLowerCase.
-        int[] alphabetArray = new int[26];
-        for ( int i = 0; i < input.length(); i++ ) {
-            char ch =  input.charAt(i);
-            int value = (int) ch;
-            if (value >= 97 && value <= 122){
-                alphabetArray[ch-'a']++;
-            }
-        }
-
-        for (int i = 0; i < alphabetArray.length; i++) {
-            if(alphabetArray[i] > 0){
-                char ch = (char) (i + 97);
-                System.out.println(ch + "  : " + alphabetArray[i]);   //Show the result.
-            }
-        }
-    }
-
-
-
-    public static void searchDigitPlusOne()
-    {
-        int[] values = {00131551, 455444412, 847777, 56211, 455645, 55411, 12215, 8412377, 521211, 453345, 411, 2345};
-        for(int value : values)
-        {
-            int[] valuesArray = getCharsOfInteger(value);
-            int[] templateArray =  new int[valuesArray.length];
-            templateArray = Arrays.copyOfRange(valuesArray, 0, valuesArray.length);
-            bubbleSort(templateArray);
-
-            if(Arrays.equals(templateArray, valuesArray) ) {System.out.println(value); break;};
-
-        }
-
-    }
-
-    public static int[] getCharsOfInteger(int number) // Integer is divided into separate elements
-    {
-        int[] elements = new int[getCountsOfDigits(number)];
-        for (int i = 0; i < elements.length ; i++)
-        {
-            elements[elements.length - i - 1] =  number % 10;
-            number /= 10;
-        }
-
-        return elements;
-    }
-
-
-    public static void bubbleSort(int[] values) // sort form min to max
-    {
-        int tmp = 0;
-        for (int i = values.length; i > 0 ; i--)
-        {
-            for (int j = 0; j < values.length - 1 ; j++)
-            {
-                if(values[j] > values[j + 1 ])
-                {
-                    tmp = values[j +1 ];
-                    values[j + 1] = values[j];
-                    values[j] = tmp;
-                }
-            }
-        }
-    }
-
-
-    public static int getCountsOfDigits(int number) // Count number of char in Integer
-    {
-        int count = (number == 0) ? 1 : 0;
-        while (number != 0)
-        {
-            count++;
-            number /= 10;
-        }
-        return count;
-    }
 }
 
 
--- src/main/java/OptionalTask2.java
+++ src/main/java/OptionalTask2.java
@@ -9,7 +9,7 @@ public class OptionalTask2 {
     public OptionalTask2() {
     }
 
-    public void firstMaxMin2_1(){  //2.1 task
+    public void firstMaxMin2_1_2(){  //2.1 : 2.2 task
         int first = 0;
         int minNum = 0;
         int maxLenth = 0;
@@ -64,7 +64,7 @@ public class OptionalTask2 {
         }
     }
 
-    public void firstMaxMin2_3(){  //2.3 task
+    public void MidLenMin2_3(){  //2.3 task
         int digt = 0;
         int mid = 0;
 
@@ -150,5 +150,103 @@ public class OptionalTask2 {
             System.out.println(s[minIndex] + " << min value");   //Show the result.
             System.out.println("---------------");   //Show the result.
 
+    }
+
+    public void increasingNumbers2_6() {
+        int[] massiv = {1589, 4412, 477, 1234, 45645, 55411, 12215, 84127, 51211, 4545, 411, 2345};
+        int[] numbers = {234, 5425, 4567, 5425, 24444, 546, 7566, 24432, 5345, 435,  411, 8675};
+        int chisloNaVhode = massiv.length;
+        int p;
+        boolean b;
+        for (int i = 0; i < chisloNaVhode; i++) {
+            int k = 0;
+            b = false;
+            int[] array1 = new int[numbers[i]];
+            p = massiv[i];
+            while (p != 0) {
+                array1[k] = p % 10;
+                p = p / 10;
+                if (array1[k] > p % 10) {
+                    b = true;
+                } else {
+                    b = false;
+                    break;
+                }
+                k++;
+            }
+            if (b == true) {
+                System.out.println("Increasing number: " + massiv[i] + ";");
+                // System.out.println();
+            }
+        }
+        System.out.println("--------------------------------");
+    }
+
+    public void differentDigitFind2_7(){
+
+        String s[] = {"1234", "455444412", "847777", "56211", "455645", "55411", "1115", "856", "541", "1115"};
+        int[] countDigit = new int[s.length];
+        int checker = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            s[i].replaceAll("\\D+", "");
+            int[] alphabetArray = new int[9];
+
+            for (int m = 0; m < s[i].length(); m++) {  //count different digits
+                char ch = s[i].charAt(m);
+                int value = (int) ch;
+                if (value >= 48 && value <= 57) {
+                    alphabetArray[ch - '0']++;
+                    if (alphabetArray[ch - '0'] > 0 ) {
+                        countDigit[i]++;
+                    }
+                }
+                for (int r = 0; r < alphabetArray.length; r++) {    // check if digit is more one
+                    if (alphabetArray[r] < 2 && alphabetArray[r] > 0 ) {
+                        checker++;
+                    }
+                }
+
+                if (s[i].length() == checker ) {
+                    System.out.println( s[i]  + "  :  << different digits" );   //Show different
+                }
+                checker = 0;
+            }
+        }
+        System.out.println("---------------");
+    }
+
+
+    public void characterFindtemp(){
+
+        String s = "aaaaabkjvbvbvbzjfdhvjfb fdjbv kjfvdbdvbjbfdjkvnkjjhlkglkhsfdjghfkjdhfdnkjvnk";
+        ArrayList<String>  strings = new ArrayList<>();
+        String temp = "";
+        Character character = 'b';
+        for (int i = 0; i < s.length(); i++) {
+//            character = s.charAt(i);
+            if(character == s.charAt(i)){
+                temp += s.charAt(i);
+                //s.
+            }
+            // System.out.println(temp + "  " + temp.length() + " >>" + temp.charAt(0));
+        }
+
+        String input = s.toLowerCase();// Make your input toLowerCase.
+        int[] alphabetArray = new int[26];
+        for ( int i = 0; i < input.length(); i++ ) {
+            char ch =  input.charAt(i);
+            int value = (int) ch;
+            if (value >= 97 && value <= 122){
+                alphabetArray[ch-'a']++;
+            }
+        }
+
+        for (int i = 0; i < alphabetArray.length; i++) {
+            if(alphabetArray[i] > 0){
+                char ch = (char) (i + 97);
+                System.out.println(ch + "  : " + alphabetArray[i]);   //Show the result.
+            }
+        }
     }
 }
