30acd4cf52958a9d034f6bb7875050b4fecb34d8
willu2 <bundestag2@gmail.com>
2019-05-29 21:33:28 +0300

    add part four matrix

--- src/main/java/OptionalMain.java
+++ src/main/java/OptionalMain.java
@@ -10,23 +10,23 @@ public class OptionalMain {
     public static void main(String[] args) {
 
        OptionalTask1 task1 = new OptionalTask1();
-       // task1.firstNameInput();
-       // task1.secondReversInput();
-       // task1.thirdRandomNum();
-       // task1.firstMaxMin2_1();
+        task1.firstNameInput();
+        task1.secondReversInput();
+        task1.thirdRandomNum();
+        task1.forthDigitSum();
 
 //===============================================================//
         OptionalTask2 task2 = new OptionalTask2();
-        //task2.firstMaxMin2_1_2();
-        //task2.MidLenMin2_3();
-        //task2.digitMinFind2_4();
-        //task2.twinDigitFind2_5temo();
-        //task2.increasingNumbers2_6();
-        //task2.differentDigitFind2_7();
+        task2.firstMaxMin2_1_2();
+        task2.MidLenMin2_3();
+        task2.digitMinFind2_4();
+        task2.twinDigitFind2_5temo();
+        task2.increasingNumbers2_6();
+        task2.differentDigitFind2_7();
 
 
-        //OptionalTask3 task3 = new OptionalTask3();
-        //task3.monthInput();
+        OptionalTask3 task3 = new OptionalTask3();
+        task3.monthInput();
 
         OptionalTask4 task4 = new OptionalTask4();
         //task4.matrix();
